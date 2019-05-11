9810e4d972b15278221cd5059eb308a98dd03cb5
Andrew Nelson <evildarkarchon@gmail.com>
2019-05-11 15:34:19 -0700

    Add Initial files along with a file with a couple hash calculation functions.
    Main file still is hello world, but just getting everything into git.

---
+++ sqlite_archive_cs/Hash.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace sqlite_archive_cs
+{
+    public class Hash {
+        private SHA256 Sha256 = SHA256.Create();
+        private SHA512 Sha512 = SHA512.Create();
+
+        public byte[] GetHashSha256(string filename)
+        {
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                return Sha256.ComputeHash(stream);
+            }
+        }
+
+        public byte[] GetHashSha512(string filename)
+        {
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                return Sha512.ComputeHash(stream);
+            }
+        }
+    }
+}
---
+++ sqlite_archive_cs/SQLite_Archive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using System.IO;
+//using System.Security.Cryptography;
+//using System.Linq;
+//using System.Text;
+//using System.Collections.Generic;
+
+//using System.Data.SQLite;
+
+namespace sqlite_archive_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
