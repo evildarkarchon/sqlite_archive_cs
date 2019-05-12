2af276986a5533b9cadc0ffc8d6f6bccf8fe9658
Andrew Nelson <evildarkarchon@gmail.com>
2019-05-11 23:24:00 -0700

    Change the base hash objects to readonly

--- sqlite_archive_cs/Hash.cs
+++ sqlite_archive_cs/Hash.cs
@@ -4,8 +4,8 @@ using System.Security.Cryptography;
 namespace sqlite_archive_cs
 {
     public class Hash {
-        private SHA256 Sha256 = SHA256.Create();
-        private SHA512 Sha512 = SHA512.Create();
+        private readonly SHA256 Sha256 = SHA256.Create();
+        private readonly SHA512 Sha512 = SHA512.Create();
 
         public byte[] GetHashSha256(string filename)
         {
