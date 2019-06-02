9903a040d3c4c044cfcbd4ba5e941caf127b50e3
Andrew Nelson <evildarkarchon@gmail.com>
2019-06-02 15:25:24 -0700

    Initial code for table name inference for if an explicit table name is not specified on the command line.

---
+++ sqlite_archive_cs/InferTable.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+namespace sqlite_archive_cs
+{
+    public class Table
+    {
+        public string Clear(ref string input)
+        {
+            return input.ToLower();
+        }
+        public string Clear(ref string input, bool lowercase)
+        {
+            string _Out = input.Replace('.', '_').Replace(' ', '_').Replace('\'', '_').Replace(',', '"').Replace('/', '_').Replace('\\', '_');
+
+            if (lowercase)
+            {
+                return _Out.ToLower();
+            }
+            else
+            {
+                return _Out;
+            }
+        }
+        public string Infer(ref string input, bool extract)
+        {
+            return ""; //Temporary to shut intellisense up.
+        }
+    }
+}
