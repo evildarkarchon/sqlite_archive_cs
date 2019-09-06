99dde4386f38129e4f7eeb8c4018ab2d35e24e9f
Andrew Nelson <evildarkarchon@gmail.com>
2019-09-05 20:28:53 -0700

    Add InferTableAdd and InferTableExtract functions and overrides.

--- sqlite_archive_cs/InferTable.cs
+++ sqlite_archive_cs/InferTable.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using System.Collections.Generic;
+using System.IO;
 namespace sqlite_archive_cs
 {
     class InferTable
     {
-        public string Clean(ref string input)
+        public string Clean(string input)
         {
             string Out = input.Replace(".", "_").Replace(" ", "_").Replace(@"'", "_").Replace(",", "").Replace("/", "_").Replace(@"\", "_").Replace(",", "_").Replace("#", "");
             return Out;
         }
-        public string Clean(ref string input, bool lowercase)
+        public string Clean(string input, bool lowercase)
         {
             string Out = input.Replace(".", "_").Replace(" ", "_").Replace(@"'", "_").Replace(",", "").Replace("/", "_").Replace(@"\", "_").Replace(",", "_").Replace("#", "");
             if (lowercase)
@@ -22,6 +20,158 @@ namespace sqlite_archive_cs
             {
                 return Out;
             }
+        }
+
+        public string InferTableAdd(ref List<string> files, bool lower)
+        {
+            string f = default;
+            bool _dirorfile = (bool)FileInfo.IsDirOrFile(files[0]);
+            if (!_dirorfile)
+            {
+                if (File.Exists(files[0]))
+                {
+                    f = Path.GetFileName(files[0]);
+                }
+            }
+            else if (_dirorfile)
+            {
+                if (Directory.Exists(files[0])) {
+                    f = Path.GetDirectoryName(files[0]);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(f))
+            {
+                return Clean(f, lower);
+            }
+            else
+            {
+                new InvalidDataException("No table name returned");
+            }
+
+            return null;
+        }
+        public string InferTableAdd(ref List<string> files)
+        {
+            string f = default;
+            bool _dirorfile = (bool)FileInfo.IsDirOrFile(files[0]);
+            if (!_dirorfile)
+            {
+                if (File.Exists(files[0]))
+                {
+                    f = Path.GetFileName(files[0]);
+                }
+            }
+            else if (_dirorfile)
+            {
+                if (Directory.Exists(files[0]))
+                {
+                    f = Path.GetDirectoryName(files[0]);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(f))
+            {
+                return Clean(f, false);
+            }
+            else
+            {
+                new InvalidDataException("No table name returned");
+            }
+
+            return null;
+        }
+
+        public string InferTableExtract(ref List<string> files, string outdir, bool lower, bool popfile)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f;
+                if (string.IsNullOrEmpty(outdir))
+                {
+                    f = Clean(Path.GetFileNameWithoutExtension(files[0]), lower);
+                }
+                else
+                {
+                    f = Clean(outdir);
+                }
+
+                if (popfile)
+                {
+                    files.Remove(files[0]);
+                }
+                return f;
+            }
+            return null;
+        }
+        public string InferTableExtract(ref List<string> files, bool lower, bool popfile)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f = Clean(Path.GetFileNameWithoutExtension(files[0]), lower);
+                if (popfile)
+                {
+                    files.Remove(files[0]);
+                }
+                return f;
+            }
+            return null;
+        }
+        public string InferTableExtract(ref List<string> files, bool popfile)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f = Clean(Path.GetFileNameWithoutExtension(files[0]), false);
+                if (popfile)
+                {
+                    files.Remove(files[0]);
+                }
+                return f;
+            }
+            return null;
+        }
+        public string InferTableExtract(ref List<string> files)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f = Clean(Path.GetFileNameWithoutExtension(files[0]), false);
+                return f;
+            }
+            return null;
+        }
+        public string InferTableExtract(ref List<string> files, string outdir, bool lower)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f;
+                if (string.IsNullOrEmpty(outdir))
+                {
+                    f = Clean(Path.GetFileNameWithoutExtension(files[0]), lower);
+                }
+                else
+                {
+                    f = Clean(outdir);
+                }
+                return f;
+            }
+            return null;
+        }
+        public string InferTableExtract(ref List<string> files, string outdir)
+        {
+            if (!string.IsNullOrEmpty(files[0]))
+            {
+                string f;
+                if (string.IsNullOrEmpty(outdir))
+                {
+                    f = Clean(Path.GetFileNameWithoutExtension(files[0]), false);
+                }
+                else
+                {
+                    f = Clean(outdir);
+                }
+                return f;
+            }
+            return null;
         }
     }
 }
